2d51ff7e211073054440bb19efd91310a1e02f06
chobers <allancho994@gmail.com>
2020-09-24 00:38:11 -0700

    added sql connection

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
 # bot.py
+import discord
 import os
 import random
+import mysql.connector
 from pokemondb import Pokemon
-from pokemondb import getName
-from pokemondb import getSprite
-
-import discord
+from pokemondb import *
 from dotenv import load_dotenv
-
 from discord.ext import commands
+
 
 pokemon = Pokemon()
 
@@ -19,54 +18,79 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 # takes name guild server 
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
-# connects bot to server and determine who is in the server 
-@client.event
-async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+HOST = os.getenv('HOST')
+USER = os.getenv('USER')
+PASS = os.getenv('PASS')
+DATABASE = os.getenv('DATABASE')
 
-
-
-# tracks messages in chat 
-@client.event
-async def on_message(message):
-    # determines if the message is from bot 
-    if message.author==client.user:
-        return
-
-    if message.content.lower() == "!play":
-        response = "Let's Play Pokemon"
-        # await message.channel.send(response) 
-        await bot.process_commands(response) 
-    # # how to raise exception
-    # elif message.content == 'raise-exception':
-    #     raise discord.DiscordException  
-
+# initalizes bot command as !
 bot = commands.Bot(command_prefix='!')
-
 
 @bot.command(name="play")
 async def play(ctx):
 
+    name = str(ctx.message.author)
+    newName = name.replace("#", '')
+    # print(newName)
+
+    mydb = mysql.connector.connect(
+    host=HOST,
+    user=USER,
+    password=PASS,
+    database=DATABASE
+    )
+
+    mycursor = mydb.cursor()
+    try:
+        mycursor.execute("CREATE TABLE "+newName+" (name VARCHAR(255))")
+        await ctx.send(newName + " has arrived!")
+    except:
+        await ctx.send(newName + " has arrived!")
+
+
+
     # displays pokemon name and sprite
     poke = pokemon.getPokemon()
     
-    response = "Let's play Pokemon!"
+    response = "Let's play Pokemon!\n!Catch pokemon you see."
     name = getName(poke)
+    names = "A wild " + name + " has appeared!"
     sprite = getSprite(poke)
     await ctx.send(response)
     await ctx.send(sprite)
-    await ctx.send(name)
-
-    
-    
-
-
+    await ctx.send(names)
 
 # need this so bot can run
 # client.run(TOKEN)
 bot.run(TOKEN)
+
+
+
+
+
+
+
+# # tracks messages in chat 
+# @client.event
+# async def on_message(message):
+#     # determines if the message is from bot 
+#     if message.author==client.user:
+#         return
+
+#     if message.content.lower() == "!play":
+#         response = "Let's Play Pokemon"
+#         # await message.channel.send(response) 
+#         await bot.process_commands(response) 
+#     # # how to raise exception
+#     # elif message.content == 'raise-exception':
+#     #     raise discord.DiscordException  
+
+# client = discord.Client()
+# # connects bot to server and determine who is in the server 
+# @client.event
+# async def on_ready():
+#     guild = discord.utils.get(client.guilds, name=GUILD)
+#     print(
+#         f'{client.user} is connected to the following guild:\n'
+#         f'{guild.name}(id: {guild.id})'
+#     )
