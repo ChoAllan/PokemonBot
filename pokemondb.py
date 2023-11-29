5a5b3bac49cc36cc56372ca5674f4fa68b0461ee
Allan Cho <allancho994@gmail.com>
2023-11-28 19:00:00 -0800

    pokemondb only generates json data files

--- pokemondb.py
+++ pokemondb.py
@@ -6,37 +6,23 @@ from urllib.request import urlopen, Request
 
 # randomly generates a pokemon from api
 class Pokemon:
-    def __init__(self):
-        self.pb = pb
-
-    def getPokemon(self):
-        number = random.randrange(0, 808)
-        pokemon = self.pb.pokemon(number)
-        return pokemon
-
-# returns the name of the pokemon generated
-def getName(pokemon):
-        pokemon
+        def __init__(self):
+                self.pb = pb
 
 
-# returns the sprite of the pokemon generated 
-def getSprite(pokemon):
-        return pokemon[0].get('sprite')
+        def getPokemon(self):
+                number = random.randrange(0, 808)
+                pokemon = self.pb.pokemon(number)
+                url = pokemon.url
 
-# # testing to generate pokemon
-pokemon = Pokemon()
-poke = pokemon.getPokemon()
-url = poke.url
-print(url)
+                req = Request(
+                        url=url, 
+                        headers={'User-Agent': 'Mozilla/5.0'}
+                        )
+                response = urlopen(req)
 
-req = Request(
-    url=url, 
-    headers={'User-Agent': 'Mozilla/5.0'}
-)
-response = urlopen(req)
+                data_json = json.loads(response.read()) 
 
-data_json = json.loads(response.read()) 
+                return data_json
 
-print(data_json) 
-# print(getName(poke))
-# print(getSprite(poke))
+
